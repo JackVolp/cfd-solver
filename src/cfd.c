299206889253ae3a0edb60466e1571490fde9c46
JTV <jvolponi0552@sdsu.edu>
2026-04-06 14:11:36 -0700

    merged with problem 1, added source term

--- src/cfd.c
+++ src/cfd.c
@@ -16,17 +16,11 @@
 int main(void)
 {	
 	/*----------------------Grid Input Filename------------------*/
-<<<<<<< HEAD
-	const char* filename = "C:\\Users\\jtvol\\Documents\\ME696\\Convection-Diffusion\\out\\build\\x64-Debug\\gmsh_grid.vtk"; //Home PC path
-	//const char* filename = "C:\\Users\\jvolponi0552\\Documents\\GitHub\\cfd-solver\\gmsh_grid.vtk"; //Lab PC path
-	//const char* filename = "C:\\Users\\jvolponi0552\\Documents\\GitHub\\cfd-solver\\1dN16.vtk"; // problem 1 grid 1
-=======
 	//const char* filename = "C:\\Users\\jtvol\\Documents\\ME696\\Convection-Diffusion\\out\\build\\x64-Debug\\gmsh_grid.vtk"; //Home PC path
 	//const char* filename = "C:\\Users\\jvolponi0552\\Documents\\GitHub\\cfd-solver\\gmsh_grid.vtk"; //Lab PC path
 	const char* filename = "C:\\Users\\jvolponi0552\\Documents\\GitHub\\cfd-solver\\1dN128.vtk"; // problem 1 grid 1
 	//const char* filename = "C:\\Users\\jtvol\\Documents\\ME696\\Convection-Diffusion\\out\\build\\x64-Debug\\1dN16.vtk";
 
->>>>>>> problem-1
 	/*-----------------------------------------------------------*/
 	// Output file name
 	//onst char* out_fname = "output_file.vtk";
